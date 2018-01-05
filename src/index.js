aee1d3bc8c98dc1f4ec6079b9e9bbeae8e4eaae7
moeabdol <mohd.a.saed@gmail.com>
2018-01-05 17:42:26 +0300

    Implement basic page structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyAzJfN6Yals7lqBym9MO6aG_8b4jRkVGOA';
 
-const App = () => {
-  return <div>Hi</div>;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, data => {
+      this.setState({ videos: data });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
